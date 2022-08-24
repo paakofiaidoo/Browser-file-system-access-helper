2dc646642d2de0956ea4a0a461ca68546375074d
Paa-Kofi Aidoo <paakofiaidoo17@gmail.com>
2022-08-24 21:33:28 +0000

    fixed export

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-let hasFileSystemAccess = !!window.showOpenFilePicker;
+// @ts-ignore
+export const hasFileSystemAccess = () => !!window.showOpenFilePicker;
 
-exports.hasFileSystemAccess = hasFileSystemAccess;
+// Language: javascript
+
+export async function getFile(options = {}) {
+    if (hasFileSystemAccess()) {
+        let [fileHandle] = await window.showOpenFilePicker(options);
+        const file = await fileHandle.getFile();
+        return file;
+    }
+}
